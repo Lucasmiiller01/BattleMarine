9f43661140a7f0a1abc12193fe78f9d22426b934
Lucasmiillers22 <lucasmiiller00007@hotmail.com>
2015-11-20 14:38:37 -0200

    Versao 0.3
    -  Add Function to instantiate the boats

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject[] Ships;
+	public Transform[] positionsShips;
+
+	void Start(){
+		StartRound ();
+	}
+
+	public void StartRound() {
+
+			for(int i = 0, f = 0; i < 8; i++, f++)
+			{
+			if(i.Equals(0) || i.Equals(2)|| i.Equals(4)|| i.Equals(6)|| i.Equals(8)) Instantiate(Ships[0], positionsShips[f].position, Quaternion.identity);
+			
+				else Instantiate(Ships[1], positionsShips[f].position, Quaternion.identity);
+
+			}
+	}
+}
